e02309e00d3f7b94e18d943757b9c5c6c9c00402
ArturOle <artur.oleksinski99@gmail.com>
2022-06-03 13:23:35 +0200

    Szkielet bazy

--- datasets/SQLQuery_1.sql
+++ datasets/SQLQuery_1.sql
@@ -1,13 +1,14 @@
+
+
 CREATE TABLE Revenue(
     song_id int,
     song_prop_id int,
     pricing_id int,
     date_id int,
     artist_id int,
-
-    profit int,  
+    profit int,
     streams int,
-    song_name VARCHAR,
+    song_name varchar(32),
 
     FOREIGN KEY (song_prop_id) REFERENCES SongProps(song_prop_id),
     FOREIGN KEY (date_id) REFERENCES Dates(date_id),
@@ -18,8 +19,8 @@ CREATE TABLE Revenue(
 
 CREATE TABLE Artists(
     artist_id int not null,
-    artist_name varchar,
-    main_genre varchar,
+    artist_name varchar(32) not null,
+    main_genre varchar(16) not null,
     PRIMARY KEY (artist_id)
 )
 
@@ -30,24 +31,25 @@ CREATE TABLE SongProps(
     dB int,
     live int,
     bmp int,
-    genre_1 varchar not NULL,
-    genre_2 varchar,
-    genre_3 VARCHAR,
+    genre_1 varchar(16) not NULL,
+    genre_2 varchar(16),
+    genre_3 varchar(16),
+    release_date datetime,
     PRIMARY KEY (song_prop_id)
 )
 
-CREATE TABLE Dates(
-    date_id int NOT NULL,
-    year int,
-    month int,
-    day int,
-    PRIMARY KEY (date_id)
-)
+-- CREATE TABLE Dates(
+--     date_id int NOT NULL,
+--     year int,
+--     month int,
+--     day int,
+--     PRIMARY KEY (date_id)
+-- )
 
 CREATE TABLE Tops(
     song_id int not null,
-    song_position int,
-    region varchar,
+    song_position int not null,
+    region varchar(32),
     creation_date datetime,
     PRIMARY KEY (song_id)
 )
